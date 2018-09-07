b2947126995251f3aff60b31eda3ab551cd7be56
Jess <jesu0404@colorado.edu>
2018-09-07 16:54:01 -0600

    new game version

---
+++ First VR Program/Assets/buttonClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonClick : MonoBehaviour {
+
+    public ballPrefab ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            ballPrefab ball = Instantiate<ballPrefab>(ballPrefab);
+            ball.transform.localPosition = transform.position;
+            ball.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * Random.Range(1000, 1250));
+        }
+	}
+}
